cabbca5ed1f246d12eb2cf8f504c0f85d4d71417
JestorNogueiro <jestornogueiro789@gmail.com>
2021-08-23 15:11:38 +0530

    footer

---
+++ components/Footer/index.js
@@ -0,0 +1,15 @@
+function Footer() {
+  return (
+    <footer className="bg-gray-400 max-h-full px-6 py-4">
+      <div>
+        <div>About</div>
+        <div> How Airbnb works </div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
