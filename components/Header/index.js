e5ec261d4dcbbbf883034c350d909aa8a25154bd
JestorNogueiro <jestornogueiro789@gmail.com>
2021-08-23 12:33:29 +0530

    added small card section

--- components/Header/index.js
+++ components/Header/index.js
@@ -16,7 +16,7 @@ function Header() {
         </h3>
       </div>
       {/* search section */}
-      <div className="flex px-5  items-center md:border-[0.5px] shadow-md border-black/70 rounded-full min-w-min">
+      <div className="flex px-5  items-center md:border-[0.5px] shadow-md border-black/70 rounded-full min-w-min hover:scale-105 transition duration-200">
         <input
           type="text"
           className=" outline-none flex-1 bg-transparent"
@@ -28,10 +28,14 @@ function Header() {
       </div>
 
       <div className=" flex items-center mx-3.5 space-x-3 justify-end">
-        <p className="hidden md:inline-flex ml-6">Become a host</p>
-        <BiGlobe className=" h-6" />
-        <div className=" flex items-center border p-3  rounded-full h-7 ">
-          <BiMenu className=" cursor-pointer" />
+        <p className="hidden rounded-full md:inline ml-6 hover:shadow-md hover:bg-white/70 px-4 cursor-pointer">
+          Become a host
+        </p>
+        <p className="hover:border hover:shadow-md hover:scale-200 px-4 cursor-pointer rounded-full hidden md:inline-flex">
+          <BiGlobe className="h-6  " />
+        </p>
+        <div className=" flex items-center border p-3  rounded-full h-7 hover:border hover:shadow-md hover:scale-200 ">
+          <BiMenu className=" cursor-pointer " />
           <BiUserCircle className="cursor-pointer" />
         </div>
       </div>
