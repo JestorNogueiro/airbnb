40b6f028e70dadfb6128e8f849b3c106e617422d
JestorNogueiro <jestornogueiro789@gmail.com>
2021-08-23 14:22:37 +0530

    Added Large card section

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,65 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import SmallCard from "../components/Cards/SmallCard";
+import LiveCard from "../components/Cards/LiveCard";
+import LargeCard from "../components/Cards/LargeCard";
 
-export default function Home() {
+export default function Home({ smallCardData, livecardData }) {
+  // console.log(exploreData);
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Airbnb || Jestor</title>
+        <link rel="icon" href="/images/airbnb-l.png" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+      <Header />
+      <Hero />
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
+      {/*  */}
+      <main className="px-8 max-w-7xl mx-auto sm:px-16">
+        <section className="pt-6">
+          <h1 className="font-semibold text-4xl">Explore nearBy</h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
+            {smallCardData?.map((item) => (
+              <SmallCard
+                key={item.distance}
+                img={item.img}
+                distance={item.distance}
+                location={item.location}
+              />
+            ))}
+          </div>
+        </section>
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
+        <section className="my-4">
+          <h2 className="text-4xl font-semibold">Live Anywhere</h2>
+          <div className="flex overflow-x-scroll overflow-y-hidden scrollbar-hide  p-3 -mx-3 ">
+            {livecardData?.map((item) => (
+              <LiveCard key={item.title} title={item.title} img={item.img} />
+            ))}
+          </div>
+        </section>
 
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <LargeCard />
       </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
     </div>
-  )
+  );
+}
+
+export async function getStaticProps(context) {
+  const smallCardData = await fetch(`https://jsonkeeper.com/b/4G1G`).then(
+    (res) => res.json()
+  );
+  const livecardData = await fetch(`https://jsonkeeper.com/b/VHHT`).then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      smallCardData,
+      livecardData,
+    },
+  };
 }
