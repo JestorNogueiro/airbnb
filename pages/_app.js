cd610a1be6edf30d5c942b022a550f37b9dcb435
JestorNogueiro <jestornogueiro789@gmail.com>
2021-08-25 01:50:46 +0530

    added bar of progress

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
 import "tailwindcss/tailwind.css";
 import "../styles/global.css";
+import Router from "next/router";
+import ProgressBar from "@badrap/bar-of-progress";
+
+const progressBar = new ProgressBar({
+  size: 4,
+  color: "#FE595E",
+  className: "z-50",
+  delay: 1,
+});
+Router.events.on("routeChangeStart", progressBar.start);
+Router.events.on("routeChangeComplete", progressBar.finish);
+Router.events.on("routeChangeError", progressBar.finish);
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
