86183f25e5e2957d4a15d964ea1f339616c93ce3
JestorNogueiro <jestornogueiro789@gmail.com>
2021-08-26 00:01:22 +0530

    added MAPBOX

---
+++ components/Map/index.js
@@ -0,0 +1,42 @@
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import { useState } from "react";
+import getCenter from "geolib/es/getCenter";
+
+function Map({ searchResult }) {
+  const coordinates = searchResult.map((result) => ({
+    longitude: result.long,
+    latitude: result.lat,
+  }));
+  const center = getCenter(coordinates);
+
+  const [viewPort, setViewPort] = useState({
+    width: "100%",
+    height: "100%",
+    latitude: center.latitude,
+    longitude: center.longitude,
+    zoom: 11,
+  });
+  return (
+    <ReactMapGL
+      mapStyle="mapbox://styles/jestor/cksr9475e1zj418quo62icq8g"
+      mapboxApiAccessToken={process.env.mapbox_key}
+      {...viewPort}
+      onViewportChange={(nextViewport) => setViewPort(nextViewport)}
+    >
+      {searchResult.map((result) => (
+        <div key={result.long}>
+          <Marker
+            longitude={result.long}
+            latitude={result.lat}
+            offsetLeft={-20}
+            offsetTop={-10}
+          >
+            <p>📌📍</p>
+          </Marker>
+        </div>
+      ))}
+    </ReactMapGL>
+  );
+}
+
+export default Map;
