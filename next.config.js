34cfc78da115197052875267fc73e5239102601b
JestorNogueiro <jestornogueiro789@gmail.com>
2021-08-25 23:59:03 +0530

    added env public mapbox key

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ["links.papareact.com"],
   },
+  env: {
+    mapbox_key:
+      "pk.eyJ1IjoiamVzdG9yIiwiYSI6ImNrc3I4bnl5czBrZW8yb282MGRxdTZ0YXcifQ.rxWvpE6i-Sb0wm6400vg-g",
+  },
 };
